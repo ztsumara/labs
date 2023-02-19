949827065619cc200e51961c7fc158b4d5bc02a5
Ztsumara <yaganoff2014@gmail.com>
2023-02-19 14:28:56 +0300

    new file:   main1.js

---
+++ main1.js
@@ -0,0 +1,12 @@
+function one(x1, s){
+    if (s=="toMS"){
+        let y1=x1/3.6;
+        console.log(x1," км/ч соответсвтует ",y1," м/с");
+    }
+    if(s=="toKMH"){
+        let y2=x1*3.6;
+        console.log(x1," м/с соответсвтует ",y2," км/ч");
+    }
+   
+   
+}
